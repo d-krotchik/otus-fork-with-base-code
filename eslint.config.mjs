fba40235d394a50f3e51094d71095e93ffc8f7d9
kurtis <d.krotchik@bftcom.com>
2025-12-07 23:06:01 +0300

    Домашнее задание - Сборка, деплой, github pages
    - правки по замечаниям ПР.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,8 @@ import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  { files: ["./*/*.js"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } }, [globalIgnores(["coverage/*", "./*/*.test.js"])]
+  { files: ["./*/*.js"], plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser } },
+  [globalIgnores(["coverage/*", "./*/*.test.js", "dist/*"])]
 ]);
