a56973fbca2c803af642d2218444bd28f1e5ce51
kurtis <d.krotchik@bftcom.com>
2025-12-22 14:29:07 +0300

    Домашнее задание - Запросы и местоположения в приложении "Погода"
    - добавлено определение местоположение пользователя и выдача прогноза по нему.

--- src/app.js
+++ src/app.js
@@ -9,15 +9,17 @@ export function weatherApp(el) {
           required
           autofocus
         />
-        <button>Получить прогноз погоды</button>
+        <button>Получить прогноз погоды по названию города</button>
+        <button type="button" data-name="geo">Получить прогноз по геолокации</button>
       </form>
-      
       <div id="weather-info"></div>
     </div>
   `;
 
   const formEl = document.querySelector("form");
   const weatherInfoEl = document.querySelector("#weather-info");
+  const geoBtn = document.querySelector("[data-name='geo']");
+  const APP_ID = "97d93f1704dcb8e35dd2045c8e75710d";
 
   function showWeather(el, weatherInfo) {
     if(weatherInfo["cod"] !=="404" ) {
@@ -36,12 +38,34 @@ export function weatherApp(el) {
   }
 
   async function getWeather(cityName) {
-    const APP_ID = "97d93f1704dcb8e35dd2045c8e75710d";
     let response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${APP_ID}`
     );
     return await response.json();
   }
+
+  geoBtn.addEventListener('click', async () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async function(position) {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          const response = await fetch(
+            `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${APP_ID}`
+          );
+          const positionInfo = await response.json();
+          let cityName = positionInfo[0].name;
+          const weatherInfo = await getWeather(cityName);
+          showWeather(weatherInfoEl, weatherInfo);
+        },
+        function(error) {
+          weatherInfoEl.innerHTML = `<div class="error-message">Ошибка геолокации</div>`
+        }
+      );
+    } else {
+      weatherInfoEl.innerHTML = `<div class="error-message">Геолокация не поддерживается в этом браузере.</div>`
+    }
+  });
 
   formEl.addEventListener("submit", async (e) => {
     e.preventDefault();
